00dc0c5b98ef3f52fcd668604dd71ff82a266b7a
M Abdul Hafeez <4hafeez142@gmail.com>
2026-02-15 23:21:46 +0530

    fix sign-up page issue

--- apps/user-app/app/(auth)/sign-up/page.tsx
+++ apps/user-app/app/(auth)/sign-up/page.tsx
@@ -1,13 +1,23 @@
+// import Authform from "@/components/ui/Authform";
+// import React from "react";
+
+// const SignUp = async() => {
+
+//   return (
+//     <section className="flex justify-center size-full max-sm:px-6">
+//       <Authform type="sign-up" />
+//     </section>
+//   );
+// };
+
+// export default SignUp;
+
 import Authform from "@/components/ui/Authform";
-import React from "react";
 
-const SignUp = async({ type }: { type: string }) => {
-
+export default function SignUpPage() {
   return (
     <section className="flex justify-center size-full max-sm:px-6">
       <Authform type="sign-up" />
     </section>
   );
-};
-
-export default SignUp;
+}
